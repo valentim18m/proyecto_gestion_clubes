fe7716bd2710b1eb2c4de9d94bfff3d091a66370
Valentin <valentinmaure2018@gmail.com>
2026-04-15 20:51:43 -0300

    Primer commit: Estructura inicial del proyecto

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <div className="App">
+      <h1>¡Bienvenidos al Club de Programación!</h1>
+      <p>
+        Únete a nosotros para aprender, compartir y crecer juntos en el mundo de
+        la programación.
+      </p>
+    </div>
+  );
+}
+
+export default App;
